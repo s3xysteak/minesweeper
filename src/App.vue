f881eb327ebeb3699831b432ccca37e00f25b751
s3xysteak <793744342@qq.com>
2023-11-03 16:04:10 +0800

    feat: :art: 改进美术

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,8 @@ function onReset() {
 </script>
 
 <template>
-  <div pt-3xl flex flex-col items-center>
-    <div>minesweeper</div>
+  <div all="c-gray-7 font-sans" pt-3xl flex flex-col items-center>
+    <div font-size-8>minesweeper</div>
     <div w-full flex items-center justify-center gap-x-4 h-16 mt-3xl>
       x length:
       <input
@@ -44,8 +44,10 @@ function onReset() {
         w-xm
       />
     </div>
-    <button @click="onReset" btn h-8 w-16>reset</button>
-    <div flex flex-col gap-y-1 mt-3xl mb-6xl>
+    <button @click="onReset" btn font-size-4 h-8 w-20 transition-100>
+      reset
+    </button>
+    <div @contextmenu.prevent select-none flex flex-col gap-y-1 mt-3xl mb-6xl>
       <mine-sweeper :options="options" ref="mineSweeper"></mine-sweeper>
     </div>
   </div>
--- uno.config.js
+++ uno.config.js
@@ -1,4 +1,10 @@
-import { defineConfig, presetUno, presetAttributify, presetIcons } from 'unocss'
+import {
+  defineConfig,
+  presetUno,
+  presetAttributify,
+  presetIcons,
+  presetWebFonts
+} from 'unocss'
 
 export default defineConfig({
   shortcuts: [
@@ -7,5 +13,16 @@ export default defineConfig({
       'b-0 shadow-md rounded cursor-pointer bg-gray-2 transition-100 hover:shadow-lg hover:bg-gray-1 active:bg-gray-3'
     ]
   ],
-  presets: [presetUno(), presetAttributify(), presetIcons()]
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+    presetWebFonts({
+      fonts: {
+        sans: 'DM Sans',
+        serif: 'DM Serif Display',
+        mono: 'DM Mono'
+      }
+    })
+  ]
 })
