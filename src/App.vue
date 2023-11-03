c46a8a8ca3436e381a6ebf4df65b4cb5e6857340
s3xysteak <793744342@qq.com>
2023-11-03 17:48:34 +0800

    feat: 自动换行的选项配置

--- src/App.vue
+++ src/App.vue
@@ -15,34 +15,50 @@ function onReset() {
 <template>
   <div all="c-gray-7 font-sans" pt-3xl flex flex-col items-center>
     <div font-size-8>minesweeper</div>
-    <div w-full flex items-center justify-center gap-x-4 h-16 mt-3xl>
-      x length:
-      <input
-        v-model="options.WIDTH"
-        placeholder="input x length..."
-        h-6
-        w-xm
-        type="number"
-      />
-      y length:
-      <input
-        v-model="options.HEIGHT"
-        placeholder="input y length..."
-        h-6
-        w-xm
-        type="number"
-      />
-      bomb probability:
-      <input
-        v-model="options.BOMB_PROB"
-        placeholder="input bomb probability..."
-        max="1"
-        min="0"
-        step="0.05"
-        type="number"
-        h-6
-        w-xm
-      />
+    <div
+      w-full
+      flex-wrap
+      flex
+      items-center
+      justify-center
+      gap-x-4
+      gap-y-4
+      h-16
+      mt-3xl
+    >
+      <div>
+        x length:
+        <input
+          v-model="options.WIDTH"
+          placeholder="input x length..."
+          h-6
+          w-xm
+          type="number"
+        />
+      </div>
+      <div>
+        y length:
+        <input
+          v-model="options.HEIGHT"
+          placeholder="input y length..."
+          h-6
+          w-xm
+          type="number"
+        />
+      </div>
+      <div>
+        bomb probability:
+        <input
+          v-model="options.BOMB_PROB"
+          placeholder="input bomb probability..."
+          max="1"
+          min="0"
+          step="0.05"
+          type="number"
+          h-6
+          w-xm
+        />
+      </div>
     </div>
     <button @click="onReset" btn font-size-4 h-8 w-20 transition-100>
       reset
