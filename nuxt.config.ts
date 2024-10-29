293e610b2678ac79116bfc8234ecb2dcec521ab1
s3xysteak <s3xysteak@outlook.com>
2024-10-29 10:05:14 +0800

    ci: relative baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '',
+  },
   compatibilityDate: '2024-10-28',
   devtools: { enabled: true },
   modules: [
