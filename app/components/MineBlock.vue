1525ba07aaecc7d70bdd45df4a9b8fa3e7364df9
s3xysteak <s3xysteak@outlook.com>
2024-10-30 13:38:16 +0800

    refactor

--- app/components/MineBlock.vue
+++ app/components/MineBlock.vue
@@ -1,35 +1,29 @@
 <script setup lang="ts">
-import type { MineSweeperOptions } from './types'
-import { watch } from 'vue'
+import type { MineBlockType } from './types'
 
-const { options } = defineProps<{
-  options: MineSweeperOptions
+const { faceup, bombsAround, type, bombAnimation } = defineProps<Pick<MineBlockType, 'type' | 'bombsAround' | 'faceup'> & {
+  bombAnimation: boolean
 }>()
-const emit = defineEmits([
-  'stepOnBomb',
-  'turnSafeCard',
-  'onEnd',
-])
-const block = defineModel<{
-  index: string
-  isTurned: boolean
-  isMine: number | false
-  isFlag: boolean
-  minesAround: undefined | number
-  isEnd: boolean
-}>('block', { required: true })
-const state = defineModel<{
-  index: string
-  value: {
-    index: string
-    isTurned: boolean
-    isMine: number | false
-    isFlag: boolean
-    minesAround: undefined
-    isEnd: boolean
+const emit = defineEmits<{
+  flagBombs: [number]
+  faceup: []
+  end: []
+}>()
+const clickable = defineModel<MineBlockType['clickable']>('clickable')
+
+const label = computed(() => !faceup || type === 'bomb' ? '' : (bombsAround || ''))
+
+const bombAnimateClass = ref(false)
+watch([() => faceup, () => type], ([faceup, type]) => {
+  if (faceup && type === 'bomb' && bombAnimation) {
+    bombAnimateClass.value = true
+    emit('end')
+
+    setTimeout(() => {
+      bombAnimateClass.value = false
+    }, 1000)
   }
-}[][] | undefined>('state', { required: true })
-const flagBombCardCount = defineModel<number>('flagBombCardCount', { required: true })
+})
 
 const isTouchstart = ref(false)
 function onTouchstart() {
@@ -47,120 +41,28 @@ function onPcClick() {
   onClick()
 }
 
-const bombImg = useTemplateRef('bombImg')
-
-const label = computed(() => {
-  if (!block.value.isTurned)
-    return ''
-  if (block.value.isMine)
-    return ''
-  return block.value.minesAround || ''
-})
-
 function onRightClick() {
-  if (block.value.isTurned)
+  if (faceup)
     return
-  block.value.isFlag = !block.value.isFlag
-  if (block.value.isMine) {
-    block.value.isFlag ? flagBombCardCount.value++ : flagBombCardCount.value--
+
+  const val = clickable.value === 'flag' ? 'ok' : 'flag'
+  clickable.value = val
+
+  if (type === 'bomb') {
+    val === 'flag' ? emit('flagBombs', 1) : emit('flagBombs', -1)
   }
 }
-
-async function onClick(isEnd = false) {
-  if (block.value.isTurned)
+async function onClick() {
+  if (faceup || clickable.value === 'flag')
     return
-  if (block.value.isFlag && !isEnd)
-    return
-  block.value.isTurned = true
+
+  emit('faceup')
 }
-
-/** @returns 周围8格的炸弹数量 */
-function generateMinesAround(row: string | number, col: string | number): number {
-  const x = Number(row)
-  const y = Number(col)
-
-  const offset = [
-    { x: -1, y: -1 },
-    { x: 0, y: -1 },
-    { x: 1, y: -1 },
-    { x: -1, y: 0 },
-    { x: 1, y: 0 },
-    { x: -1, y: 1 },
-    { x: 0, y: 1 },
-    { x: 1, y: 1 },
-  ]
-
-  let count = 0
-  const shouldTurn: { x: number, y: number }[] = []
-
-  const { height, width } = options
-
-  offset.forEach((i) => {
-    const neighborX = x + i.x
-    const neighborY = y + i.y
-
-    if (
-      (neighborX >= 0)
-      && (neighborX < height)
-      && (neighborY >= 0)
-      && (neighborY < width)
-    ) {
-      state.value?.[neighborX]?.[neighborY]?.value?.isMine
-        ? count++
-        : shouldTurn.push({ x: neighborX, y: neighborY })
-    }
-  })
-
-  if (count !== 0)
-    return count
-  shouldTurn.forEach((pos) => {
-    if (!state.value?.[pos.x]?.[pos.y]?.value)
-      return
-    state.value[pos.x]![pos.y]!.value.isTurned = true
-  })
-
-  return count
-}
-
-watchEffect(() => {
-  block.value.isEnd && onClick(true)
-})
-watch(
-  () => block.value.isTurned,
-  (val) => {
-    if (!val)
-      return
-
-    if (block.value.isMine) {
-      if (block.value.isEnd || !bombImg.value)
-        return
-
-      bombImg.value.animate(
-        [
-          { opacity: 1, transform: 'scale(1, 1)' },
-          { opacity: 0, transform: 'scale(50, 50)' },
-        ],
-        {
-          duration: 500,
-          easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
-        },
-      )
-      emit('stepOnBomb')
-      return
-    }
-
-    const [row, col] = block.value.index.split(':')
-    if (!row || !col)
-      return
-    block.value.minesAround = generateMinesAround(row, col)
-    emit('turnSafeCard')
-  },
-)
 </script>
 
 <template>
   <div
-    :class="block.isTurned ? 'bg-gray-1' : 'bg-gray-3 hover:bg-gray-2 cursor-pointer active:bg-gray-4'"
+    :class="faceup ? 'bg-gray-1' : 'bg-gray-3 hover:bg-gray-2 cursor-pointer active:bg-gray-4'"
     b="1 solid gray" flex="~ justify-center items-center"
     h-8 w-8 rounded font-size-5 transition-100
     @touchstart="onTouchstart"
@@ -170,13 +72,35 @@ watch(
   >
     {{ label }}
     <div
-      v-show="block.isTurned && block.isMine"
-      ref="bombImg"
-      class="i-mdi-bomb"
-    />
+      v-show="faceup && type === 'bomb'"
+      :class="{ animate: bombAnimateClass }"
+    >
+      <div i-mdi-bomb />
+    </div>
     <div
-      v-show="block.isFlag && !block.isTurned"
-      class="i-mdi-flag-outline"
+      v-show="clickable === 'flag' && !faceup"
+      i-mdi-flag-outline
     />
   </div>
 </template>
+
+<style scoped>
+.animate {
+  --uno: relative;
+}
+.animate::after {
+  content: '';
+  --uno: absolute top-0 left-0 i-mdi-bomb;
+  animation: fadeInOut 0.5s cubic-bezier(0.4, 0, 0.2, 1);
+}
+@keyframes fadeInOut {
+  0% {
+    opacity: 1;
+    transform: scale(1);
+  }
+  100% {
+    opacity: 0;
+    transform: scale(25);
+  }
+}
+</style>
--- app/components/MineSweeper.vue
+++ app/components/MineSweeper.vue
@@ -1,84 +1,138 @@
 <script setup lang="ts">
-import type { MineSweeperOptions } from './types'
+import type { MineBlockType, MineSweeperOptions } from './types'
 
 const { options } = defineProps<{
   options: MineSweeperOptions
 }>()
 
-let bombCount = 0
-const turnedSafeCardCount = ref(0)
+const bombCount = ref(0)
+const faceupSafeCardCount = ref(0)
 const flagBombCardCount = ref(0)
+const isEnd = ref(false)
 
-const state = ref<{
-  index: string
-  value: {
-    index: string
-    isTurned: boolean
-    isMine: number | false
-    isFlag: boolean
-    minesAround: undefined
-    isEnd: boolean
-  }
-}[][]>()
+const offset = [
+  { x: -1, y: -1 },
+  { x: 0, y: -1 },
+  { x: 1, y: -1 },
+  { x: -1, y: 0 },
+  { x: 1, y: 0 },
+  { x: -1, y: 1 },
+  { x: 0, y: 1 },
+  { x: 1, y: 1 },
+]
+
+const state = ref<Array<MineBlockType & { row: number, col: number }>[]>()
 
 function init() {
-  bombCount = 0
-  turnedSafeCardCount.value = 0
+  bombCount.value = 0
+  faceupSafeCardCount.value = 0
   flagBombCardCount.value = 0
+  isEnd.value = false
+
   state.value = Array.from({ length: options.height }).map(
     (_, rowIndex) =>
-      Array.from({ length: options.width }).map((_, colIndex) => ({
-        index: `${rowIndex}:${colIndex}`,
-        value: {
-          index: `${rowIndex}:${colIndex}`,
-          isTurned: false,
-          isMine: Math.random() < options.bombProb && ++bombCount,
-          isFlag: false,
-          minesAround: undefined,
-          isEnd: false,
-        },
-      })),
+      Array.from({ length: options.width }).map<MineBlockType & { row: number, col: number }>((_, colIndex) => {
+        const type = Math.random() > options.bombProb ? 'normal' : 'bomb'
+        type === 'bomb' && bombCount.value++
+        return {
+          col: colIndex,
+          row: rowIndex,
+          faceup: false,
+          type,
+          clickable: 'ok',
+          bombsAround: 0,
+        }
+      }),
   )
+
+  state.value.forEach((rows) => {
+    rows.forEach((block) => {
+      const x = Number(block.row)
+      const y = Number(block.col)
+
+      let count = 0
+
+      const { height, width } = options
+
+      offset.forEach((i) => {
+        const neighborX = x + i.x
+        const neighborY = y + i.y
+
+        if (
+          (neighborX >= 0)
+          && (neighborX < height)
+          && (neighborY >= 0)
+          && (neighborY < width)
+          && state.value?.[neighborX]?.[neighborY]?.type === 'bomb'
+        ) {
+          count++
+        }
+      })
+
+      block.bombsAround = count
+    })
+  })
 }
 
 init()
 defineExpose({ init })
 
-function onEnd() {
-  state.value?.forEach?.((row) => {
-    row.forEach((col) => {
-      col.value.isEnd = true
-    })
-  })
-}
-function onStepOnBomb() {
-  onEnd()
-}
+const safeCardCount = computed(() => options.width * options.height - bombCount.value)
 watchEffect(() => {
   if (
-    turnedSafeCardCount.value
-    === options.width * options.height - bombCount
-    && flagBombCardCount.value === bombCount
+    faceupSafeCardCount.value === safeCardCount.value
+    && flagBombCardCount.value === bombCount.value
   ) {
     setTimeout(() => {
       /* eslint-disable no-alert */
       alert('you win! 🎉')
-      onEnd()
+      isEnd.value = true
     }, 500)
   }
 })
+watch(isEnd, (end) => {
+  if (end) {
+    state.value?.forEach((rows) => {
+      rows.forEach((block) => {
+        block.faceup = true
+      })
+    })
+  }
+})
+
+function tryFaceup(item: MineBlockType & { row: number, col: number }) {
+  if (item.faceup)
+    return
+  item.faceup = true
+  if (item.type === 'normal')
+    faceupSafeCardCount.value++
+
+  if (item.bombsAround)
+    return
+
+  offset.forEach(({ x, y }) => {
+    const neighborX = item.row + x
+    const neighborY = item.col + y
+
+    const block = state.value?.[neighborX]?.[neighborY]
+    if (block)
+      tryFaceup(block)
+  })
+}
 </script>
 
 <template>
-  <div v-for="(row, i) in state" :key="i" flex="~ gap-1">
-    <div v-for="item in row" :key="item.index">
+  <div v-for="(row, i) in state" :key="i" :style="isEnd && 'pointer-events: none;'" flex="~ gap-1">
+    <div v-for="item in row" :key="`${item.col}:${item.row}`">
       <MineBlock
-        v-model:block="item.value"
-        v-model:state="state"
-        v-model:flag-bomb-card-count="flagBombCardCount"
-        :options="options"
-        @turn-safe-card="turnedSafeCardCount++"
-        @step-on-bomb="onStepOnBomb"
+        v-model:clickable="item.clickable"
+        v-model:faceup="item.faceup"
+        :bombs-around="item.bombsAround"
+        :type="item.type"
+        :bomb-animation="!isEnd"
+        @flag-bombs="(num) => flagBombCardCount += num"
+        @faceup="tryFaceup(item)"
+        @end="isEnd = true"
       />
     </div>
   </div>
