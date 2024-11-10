f9e27deec8fa2aa60b8fd587831b8752572cdc00
s3xysteak <s3xysteak@outlook.com>
2024-11-10 17:46:26 +0800

    fix: create the seed if today's seed is not exist. Because cloudflare pages do not support task yet.

--- server/api/daily-run/today-seed.get.ts
+++ server/api/daily-run/today-seed.get.ts
@@ -1,11 +1,22 @@
 export default defineEventHandler(async () => {
   const db = hubDatabase()
 
-  const value = await db
-    .prepare(`
-      SELECT id, seed FROM daily_run
-      WHERE DATE(create_time) = CURRENT_DATE;`)
-    .all()
+  const getResult = async () => {
+    const value = await db
+      .prepare(`
+    SELECT id, seed FROM daily_run
+    WHERE DATE(create_time) = CURRENT_DATE;`)
+      .all()
 
-  return value.results?.[0]
+    return value.results?.[0]
+  }
+
+  const value = await getResult()
+
+  if (!value) {
+    await runTask('insert-daily-run')
+    return await getResult()
+  }
+
+  return value
 })
